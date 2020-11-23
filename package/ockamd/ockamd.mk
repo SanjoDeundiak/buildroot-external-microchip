88f3ed6a2cac61808f3dce5e56221815590b996b
Oleksandr Deundiak <deundiak@gmail.com>
2020-11-23 16:56:59 +0200

    8

--- package/ockamd/ockamd.mk
+++ package/ockamd/ockamd.mk
@@ -6,7 +6,8 @@
 
 OCKAMD_VERSION = 0.10.1
 OCKAMD_SOURCE = ockam-$(OCKAMD_VERSION).tar.gz
-OCKAMD_SITE = $(call github,ockam-network,ockam,$(OCKAMD_VERSION))
+OCKAMD_SITE = http://www.foosoftware.org/download
+# OCKAMD_SITE = $(call github,ockam-network,ockam,$(OCKAMD_VERSION))
 OCKAMD_LICENSE = "Apache License 2.0"
 OCKAMD_LICENSE_FILES = LICENSE
 
